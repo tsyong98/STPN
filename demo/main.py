fab0f5e4f360ee3865e39072b97248cda9a3e4a0
Sin Yong Tan <tsyong98@gmail.com>
2022-03-03 15:27:42 -0600

    Updated demo main.py

--- demo/main.py
+++ demo/main.py
@@ -55,13 +55,73 @@ plt.plot(embedded_time["cos_time"])
 '''
 
 # ==========================
+
+# Use datatest.csv temperature and occ as example
+
+# Check target trimming with depth and tau
+
+# Check if the code works with depth and tau = 0
+
 import pandas as pd
-from STPN.util import *
+# from STPN.util import *
+from util import *
 
-# load timestamped_illum1.csv
-data = pd.read_csv("timestamped_illum1.csv",index_col=0)
+# Define parameters
+depth = 2
+tau = 1
 
-boundaries = discretize(data['illum'].values, mode="uniform", numparts=5)
+# Load data
+data = pd.read_csv("datatest.csv",index_col=0)
+
+# Temperature	Humidity	Light	CO2	HumidityRatio	Occupancy
+
+# Generate boundaries
+boundaries = discretize(data['Temperature'].values, mode="uniform", numparts=5)
 print(boundaries)
 
-visualize_bounds(data['illum'].values, boundaries)
+# Visualize boundaries
+# visualize_bounds(data['Temperature'].values, boundaries)
+
+# Symbolize data using defined boundaries
+symbolized_data = symbolize(data['Temperature'].values, boundaries)
+
+# Generate state(symbolized data with defined depth) using symbolized data
+state = state_gen(symbolized_data, depth=depth, tau=tau)
+# print(state)
+
+# (optional) Re-define state's element to single "symbol" representation
+# Function coming soon
+
+
+# Define prediction target
+occupancy = data['Occupancy'].values # Classification (Binary)
+# Use symbolized_data(temperature) # Regression (Multi-class Classification)
+
+# Trim state with depth (see figure for clarification)
+occupancy = occupancy[depth+tau:]
+temperature = symbolized_data[depth+tau:]
+
+assert len(temperature)==(len(data['Temperature'].values)-depth-tau)
+assert len(occupancy)==(len(data['Temperature'].values)-depth-tau)
+
+assert len(temperature)==len(state)
+assert len(occupancy)==len(state)
+
+# Construct state-occupancy Trans. Mat.
+TM_binary, TM_binary_c, binary_targets = compute_TM(state, occupancy, True)
+
+# Construct state-temperature Trans. Mat.
+TM_multi, TM_multi_c, multi_targets = compute_TM(state, temperature, True)
+
+
+# Inferencing using state-occupancy Trans. Mat.
+occ_pred = inference(state, TM_binary, binary_targets, mode="ffill", verbose=1)
+
+# Inferencing using state-temperature Trans. Mat.
+temp_pred = inference(state, TM_multi, multi_targets, mode="ffill", verbose=1)
+
+# Evaluate inference results
+occ_acc = eval_(occupancy, occ_pred, binary_targets)
+temp_acc = eval_(temperature, temp_pred, multi_targets)
+
+
